bccd4a837a9c44935caf8ea2a82ace0f3c14d803
ahmed <ahmedmijdo2@gmail.com>
2026-08-24 09:37:08 +0300

    setup tailwind css with pug

---
+++ tailwind.config.ts
@@ -0,0 +1,8 @@
+// tailwind.config.js
+module.exports = {
+    content: ["./public/**/*.css", "./views/*.pug"], // Adjust path to match your Pug file location
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}   
